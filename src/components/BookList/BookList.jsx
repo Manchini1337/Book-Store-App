91c1bef1cf9b79a366ea2cc2ebbf9161025f2205
Manchini1337 <mezureno@gmail.com>
2022-08-25 21:26:18 +0200

    display h1 tag until data fetches

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -6,16 +6,22 @@ import { Link } from 'react-router-dom';
 const BookList = ({ books }) => {
   return (
     <div className={classes.container}>
-      <div className={classes.bookGrid}>
-        {books.map((book) => (
-          <BookListItem book={book} key={book.id}>
-            {book.title}
-          </BookListItem>
-        ))}
-      </div>
-      <Link to='/cart'>
-        <button className={classes.btn}>Zobacz koszyk &rarr;</button>
-      </Link>
+      {books.length === 0 ? (
+        <h1 className={classes.loading}>Loading...</h1>
+      ) : (
+        <>
+          <div className={classes.bookGrid}>
+            {books.map((book) => (
+              <BookListItem book={book} key={book.id}>
+                {book.title}
+              </BookListItem>
+            ))}
+          </div>
+          <Link to='/cart'>
+            <button className={classes.btn}>Zobacz koszyk &rarr;</button>
+          </Link>
+        </>
+      )}
     </div>
   );
 };
